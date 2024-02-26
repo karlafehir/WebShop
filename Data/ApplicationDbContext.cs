c195c5e821389733067ff7d6f39439df77a7cff2
karlafehir <fehirkarla@gmai.com>
2024-02-26 19:47:42 +0100

    add entity framework, add category entity

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebShop.Models;
+
+namespace WebShop.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Category> Categories { get; set; }
+}
