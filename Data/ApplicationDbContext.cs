3f540a55069f7081ab9cb62ebe9770c1d4ad0c52
karlafehir <fehirkarla@gmai.com>
2024-03-04 19:43:46 +0100

    added migrations

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -11,4 +11,28 @@ public class ApplicationDbContext : DbContext
     }
 
     public DbSet<Category> Categories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>().HasData(
+            new Category
+            {
+                Id = 1,
+                Name = "SciFi",
+                DisplayOrder = 1
+            },
+            new Category
+            {
+                Id = 2,
+                Name = "Novel",
+                DisplayOrder = 2
+            },
+            new Category
+            {
+                Id = 3,
+                Name = "Thriller",
+                DisplayOrder = 3
+            }
+        );
+    }
 }
